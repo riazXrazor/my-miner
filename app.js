1dcebc3b068ec24906c7ea8202aea6f114f35907
Amitabh Roy <freelancer.amitabh@gmail.com>
2017-10-07 18:20:30 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+app.use(express.static(__dirname + '/assets'))
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(8000,function(){
+  console.log("Live at Port 8000");
+});
