a5ddb4d3ca0369f5ee57addae712f5e691295a27
Riaz Laskar <riazcool77@gmail.com>
2017-12-10 21:53:32 +0530

    animation update

--- assets/js/header-animation.js
+++ assets/js/header-animation.js
@@ -64,7 +64,7 @@
 
         // assign a circle to each point
         for(var i in points) {
-            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
+            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.5)');
             points[i].circle = c;
         }
     }
@@ -156,7 +156,7 @@
             ctx.beginPath();
             ctx.moveTo(p.x, p.y);
             ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+            ctx.strokeStyle = 'rgba(255,255,255,'+ p.active+')';
             ctx.stroke();
         }
     }
@@ -175,7 +175,7 @@
             if(!_this.active) return;
             ctx.beginPath();
             ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+            ctx.fillStyle = 'rgba(255,255,255,'+ _this.active+')';
             ctx.fill();
         };
     }
