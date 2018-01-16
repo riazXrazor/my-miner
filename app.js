158b57db7cb9f6588327827d6610803c29c38e36
Riaz Laskar <riazcool77@gmail.com>
2018-01-16 23:18:39 +0530

    telegram bot

--- app.js
+++ app.js
@@ -10,12 +10,10 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 bot.on(['/start'], (msg) => {
-    return bot.sendMessage(msg.from.id, `Welcome, ${msg.from.first_name}! \n
-    I am a telegram bot which can provide you with updated on the popular 
-    crypto currency prices in INR and keep you update to date as
+    return bot.sendMessage(msg.from.id, `Welcome, ${msg.from.first_name}!
+    I am a telegram bot which can provide you with updated on the popular crypto currency prices in INR and keep you update to date as
     and when required.
-    \n
-    - Made by http://riazxrazor.in
+    - Made by <a href="http://riazxrazor.in">Riaz Laskar</a>
     `);
 });
 
