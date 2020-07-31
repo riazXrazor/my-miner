aa7344dc1dfe1c409acd3fa23d7ca0484d92ab52
Riaz Laskar <riazcool77@gmail.com>
2020-07-31 13:45:18 +0530

    process kill condition

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -4,4 +4,13 @@ echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
-kill $(cat .pidfile)
+$proc_name = $(cat .pidfile)
+
+if pgrep $proc_name
+then
+echo " $proc_name running "
+kill $proc_name
+echo "$proc_name  got killed"
+else
+echo " $proc_name is not running/stopped "
+fi
