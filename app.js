6a48833698cfc7877ea89d86bfa26046524afb9f
Riaz Laskar <riazcool77@gmail.com>
2021-03-19 19:47:58 +0530

    minor http2 updates removed

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const sgMail = require("@sendgrid/mail");
 const tailwindo = require("tailwindo");
 const spdy = require('spdy');
 const fs = require('fs');
-
+const path = require('path');
+const url = require('url')
+const logger = require('morgan')
 
 app.set("view engine", "ejs");
 
@@ -19,12 +21,66 @@ app.use(
   })
 );
 app.use(express.static(__dirname + "/assets"));
-router.use(function (req, res, next) {
-  console.log("/" + req.method);
-  next();
-});
+app.use(logger('dev'))
+var walk = function(dir, done) {
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    var i = 0;
+    (function next() {
+      var file = list[i++];
+      if (!file) return done(null, results);
+      file = path.join(dir, file);
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            next();
+          });
+        } else {
+          results.push(file);
+          next();
+        }
+      });
+    })();
+  });
+};
+const files = {}
+walk('views',(err,data) => {
+    data.forEach(name=>{
+      if(!name.includes('partials')) return;
+      files[`${name}`]=fs
+        .readFileSync(name, {encoding: 'utf8'})
+        .split('\n')
+        .filter(line=>line.match(/src *?= *?"(.*)"/)!=null)
+        .map(line=>line.match(/src *?= *?"(.*)"/)[1])
+    })
+
+})
+
+
+
+
+
+app.get('/pushy', (req, res) => {
+  var stream = res.push('/main.js', {
+    status: 200, // optional
+    method: 'GET', // optional
+    request: {
+      accept: '*/*'
+    },
+    response: {
+      'content-type': 'application/javascript'
+    }
+  })
+  stream.on('error', function() {
+  })
+  stream.end('alert("hello from push stream!");')
+  res.render("index");
+})
 
 router.get("/", function (req, res) {
+
   res.render("index");
 });
 
@@ -118,11 +174,11 @@ app.use("/", router);
 app.use("*", function (req, res) {
   res.render("404");
 });
-var PORT = 3000;
-// app.listen(port,function(){
-//   console.log("Live at Port "+port);
-//   // bot.start();
-// });
+var PORT = process.env.PORT || 3000;
+app.listen(PORT,function(){
+  console.log("Live at Port "+PORT);
+  // bot.start();
+});
 
 /* var io = require("socket.io").listen(
   app.listen(port, function () {
@@ -138,16 +194,17 @@ io.sockets.on("connection", function (socket) {
     res.send(JSON.stringify(req.query));
   });
 }); */
-let keypath =  'keys/privkey3.pem'
-let certpath = 'keys/fullchain3.pem'
+//  let keypath =  'keys/privkey3.pem'
+//  let certpath = 'keys/fullchain3.pem'
 // if(process.env.ENV === 'PROD'){
 //  keypath = process.env.keyspath+'/privkey3.pem'
 //  certpath = process.env.keyspath+'/fullchain3.pem'
 // }
 
-spdy.createServer({
-  key: fs.readFileSync(keypath),
-  cert: fs.readFileSync(certpath)
-}, app).listen(PORT, 'localhost', () => {
-  console.log(`HTTP/2 Express running at https://localhost:${PORT}`)
-});
+
+// spdy.createServer({
+//   key: fs.readFileSync(keypath),
+//   cert: fs.readFileSync(certpath)
+// }, app).listen(PORT, 'localhost', () => {
+//   console.log(`HTTP/2 Express running at https://localhost:${PORT}`)
+// });
